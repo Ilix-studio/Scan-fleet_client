4f5c8c04533d77459986302431b732108105e47c
ilix-studio <ilishjyoti17@gmail.com>
2026-01-16 03:10:24 +0530

    vercel fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,17 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
   },
 });
